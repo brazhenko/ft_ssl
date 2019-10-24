984228f88513499aa69dce9fc77e6c927b680f3c
lreznak- <andreybrazhenko@yandex.ru>
2019-10-24 10:48:19 +0300

    done sha256 for string

--- ft_sha256/ft_sha256.c
+++ ft_sha256/ft_sha256.c
@@ -21,7 +21,7 @@ void			print_sha256_hash(t_hash_sha256 hash)
 {
 	for (int i = 0; i < 8; i++)
 	{
-		printf("%08x ", hash.hash[i]);
+		printf("%08x", hash.hash[i]);
 	}
 	printf("\n");
 }
@@ -36,29 +36,30 @@ void			print_hx(reg32* hash)
 	printf("\n");
 }
 
-
-reg32	calculate_w_param(size_t i, reg32 *ptr)
-{
-	if (i <= 15)
-	{
-		return (ptr[i]);
-	}
-	else
-	{
-		reg32 t1 = calculate_w_param(i - 2, ptr);
-		reg32 t2;
-		reg32 t3 = calculate_w_param(i - 15, ptr);
-		reg32 t4;
-
-		return (SHA256_S3(t1) + calculate_w_param(i - 7, ptr) + SHA256_S2(t3) + (calculate_w_param(i - 16, ptr)));
-	}
-}
-
 int				calculate_sha256_block(reg32 *ptr, t_hash_sha256 *hash)
 {
 	size_t		i;
 	reg32		tmp[8];
 	reg32		T1, T2;
+
+
+	//---------
+	reg32		w[64];
+	for (int i = 0 ;i < 16; i++)
+	{
+		w[i] = 0;
+		//TODO refactor this hardcode pizdec
+		w[i] |=  (ptr[i] >> 24) & 0b11111111;
+		w[i] |=  (((ptr[i] >> 16) & 0b11111111) << 8);
+		w[i] |=  (((ptr[i] >> 8) & 0b11111111) << 16);
+		w[i] |=  (((ptr[i] >> 0) & 0b11111111) << 24);
+	}
+	for (int i = 16; i < 64; i++)
+	{
+		w[i] = 	w[i-16] + SHA256_S2(w[i-15]) + w[i-7] + SHA256_S3(w[i-2]);
+	}
+	//---------
+
 
 	// TODO fix hardcode below
 	tmp[0] = hash->hash[0];
@@ -71,13 +72,18 @@ int				calculate_sha256_block(reg32 *ptr, t_hash_sha256 *hash)
 	tmp[7] = hash->hash[7];
 	//-------
 
+
 	i = 0;
 	while (i < 64)
 	{
 		// Swapping elements
-		T1 = tmp[7] + SHA256_S1(tmp[4]) + SHA256_CH(tmp[4], tmp[5], tmp[6]) + sha256_constants[i] + calculate_w_param(i, ptr);
-		T2 = SHA256_S0(tmp[0] + SHA256_MAJ(tmp[0], tmp[1], tmp[2]));
-		printf("%u\n", T2);
+		T1 = 	tmp[7] +
+				SHA256_S1(tmp[4]) +
+				SHA256_CH(tmp[4], tmp[5], tmp[6]) +
+				sha256_constants[i] +
+				w[i];
+
+		T2 = SHA256_S0(tmp[0]) + SHA256_MAJ(tmp[0], tmp[1], tmp[2]);
 		tmp[7] = tmp[6]; // h
 		tmp[6] = tmp[5]; // g
 		tmp[5] = tmp[4]; // f
@@ -86,10 +92,8 @@ int				calculate_sha256_block(reg32 *ptr, t_hash_sha256 *hash)
 		tmp[2] = tmp[1]; // c
 		tmp[1] = tmp[0]; // b
 		tmp[0] = T1 + T2; // a
-
 		i++;
 
-		print_hx(tmp);
 	}
 
 	// TODO think how to optimise this increment
@@ -102,6 +106,7 @@ int				calculate_sha256_block(reg32 *ptr, t_hash_sha256 *hash)
 	hash->hash[6] += tmp[6];
 	hash->hash[7] += tmp[7];
 	// -------
+
 	return (0);
 }
 
@@ -120,13 +125,11 @@ size_t				calculate_padding(const char *str, char	**padded)
 	bzero(*padded, padded_len);
 	memcpy(*padded, str, len);
 	(*padded)[len] = 0x80;
-	print_bit_str(*padded, padded_len);
 	(*padded)[padded_len - 4] = ((len * 8) >> 24) & 0b11111111;
 	(*padded)[padded_len - 3] = ((len * 8) >> 16) & 0b11111111;
 	(*padded)[padded_len - 2] = ((len * 8) >> 8) & 0b11111111;
 	(*padded)[padded_len - 1] = ((len * 8) >> 0) & 0b11111111;
-	print_bit_str(*padded, padded_len);
-	printf("padded string len: %lu\norig len: %lu\n", padded_len, len);
+	// printf("padded string len: %lu\norig len: %lu\n", padded_len, len);
 	return (padded_len);
 }
 
@@ -138,11 +141,9 @@ t_hash_sha256	calculate_hash_from_string(const char *str)
 
 	init_sha256_hash(&hash);
 	padded_len = calculate_padding(str, &padded);
-	print_sha256_hash(hash);
 	while (padded_len)
 	{
 		calculate_sha256_block((reg32 *)padded, &hash);
-		print_sha256_hash(hash);
 		padded += 64;
 		padded_len -= 64;
 	}
@@ -175,7 +176,8 @@ void			*sha256(char *str, int flags)
 
 	if (flags & FLAG_P || flags & FLAG_STDIN)
 	{
-		calculate_hash_from_stdin();
+		hash = calculate_hash_from_stdin();
+		print_sha256_hash(hash);
 	}
 	else if (flags & FLAG_S)
 	{
--- include/ft_sha256.h
+++ include/ft_sha256.h
@@ -6,14 +6,24 @@
 **	32-bit (8-byte) value.
 */
 
-# define SHA256_RSHIFT(x, n) ((x) >> (n)) | (((x) << (32 - (n))))
+# define SHA256_RSHIFT(x, n) (((x) >> (n)) | (((x) << (32 - (n)))))
 # define SHA256_RROTATION(x, n) ((x) >> (n))
 # define SHA256_CH(x, y, z) (((x) & (y)) ^ ((~(x)) & (z)))
 # define SHA256_MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
-# define SHA256_S0(x) ( (SHA256_RSHIFT((x), 2)) ^ (SHA256_RSHIFT((x), 13)) ^ (SHA256_RSHIFT((x), 22)) )
+
+# define SHA256_S0(x) ((SHA256_RSHIFT((x), 2)) ^ (SHA256_RSHIFT((x), 13)) ^ (SHA256_RSHIFT((x), 22)))
 # define SHA256_S1(x) ((SHA256_RSHIFT((x), 6)) ^ (SHA256_RSHIFT((x), 11)) ^ (SHA256_RSHIFT((x), 25)))
 # define SHA256_S2(x) ((SHA256_RSHIFT((x), 7)) ^ (SHA256_RSHIFT((x), 18)) ^ (SHA256_RROTATION((x), 3)))
 # define SHA256_S3(x) ((SHA256_RSHIFT((x), 17)) ^ (SHA256_RSHIFT((x), 19)) ^ (SHA256_RROTATION((x), 10)))
+
+
+
+# define ROTATE(x, n) (((x) >> (n)) | (((x) << (32 - (n)))))
+# define Sigma0(x)       (ROTATE((x),30) ^ ROTATE((x),19) ^ ROTATE((x),10))
+# define Sigma1(x)       (ROTATE((x),26) ^ ROTATE((x),21) ^ ROTATE((x),7))
+# define sigma0(x)       (ROTATE((x),25) ^ ROTATE((x),14) ^ ((x)>>3))
+# define sigma1(x)       (ROTATE((x),15) ^ ROTATE((x),13) ^ ((x)>>10))
+# define Ch(x,y,z)       (((x) & (y)) ^ ((~(x)) & (z)))
 
 typedef unsigned int reg32;
 
