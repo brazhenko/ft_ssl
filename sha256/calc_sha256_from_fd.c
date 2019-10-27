fdb83ba58f124d2b4dc3f13dd1e024418dc4df8f
lreznak- <andreybrazhenko@yandex.ru>
2019-10-27 03:07:48 +0300

    update sha256 from file, fixed

--- sha256/calc_sha256_from_fd.c
+++ sha256/calc_sha256_from_fd.c
@@ -11,66 +11,66 @@
 ** 		Returns number of bytes ADDED, NOT the len of all buffer
 */
 
-size_t				calculate_sha256_buf_padding(
-		char *padded,
-		size_t len
-)
+size_t				calculate_sha256_buf_padding(char *padded, size_t len)
 {
 	size_t		padded_len;
 	size_t		ret;
 
 	padded_len = len + 1;
-	if (len % 64 < 60)
+	ret = 0;
+	padded[len % (BUFLEN)] = 0x80;
+	if ((padded_len % 64 == 0) || (padded_len % 64 >= 60))
 	{
-		ret = 64 - len % 64;
-		padded_len = padded_len + (64 - padded_len % 64);
+		ret = 64 + (64 - len % 64);
+		padded_len = ret + len;
 	}
 	else
 	{
-		ret = 64 + (64 - len % 64);
-		padded_len = padded_len + 64 + (64 - padded_len % 64);
+		ret = 64 - len % 64;
+		padded_len = ret + len;
 	}
-
-	padded[len % (BUFLEN + 64)] = 0x80;
-	padded[(padded_len - 4)  % (BUFLEN + 64)] = ((len * 8) >> 24) & 0b11111111;
-	padded[(padded_len - 3)  % (BUFLEN + 64)] = ((len * 8) >> 16) & 0b11111111;
-	padded[(padded_len - 2)  % (BUFLEN + 64)] = ((len * 8) >> 8) & 0b11111111;
-	padded[(padded_len - 1)  % (BUFLEN + 64)] = ((len * 8) >> 0) & 0b11111111;
-	printf("PADDED: %u , a:%u, b:%u len: %u\n",
-			ret,
-			(padded_len - 4)  % (BUFLEN + 64),
-			padded_len,
-			len
-	);
-	print_bit_str("abc: ", padded, 128);
+	padded[(padded_len - 4)  % (BUFLEN)] = ((len * 8) >> 24) & 0b11111111;
+	padded[(padded_len - 3)  % (BUFLEN)] = ((len * 8) >> 16) & 0b11111111;
+	padded[(padded_len - 2)  % (BUFLEN)] = ((len * 8) >> 8) & 0b11111111;
+	padded[(padded_len - 1)  % (BUFLEN)] = ((len * 8) >> 0) & 0b11111111;
+	printf("calc_padding() ret: %lu padded_len: %lu len: %lu\n", ret, padded_len, len);
 	return (ret);
 }
 
 static int		get_block_from_fd(int fd, char **block)
 {
 	static char		buffer[BUFLEN + 64];
-	static size_t	len = 0;
 	static size_t	iter = 0;
+	static size_t	len = 0;
 	static ssize_t	rd = 0;
-	int				padd1 = 0;
+	static int		padded = 0;
 
-	if (!rd)
+	if (iter == rd)
 	{
 		bzero(buffer, BUFLEN);
 		rd = read(fd, buffer, BUFLEN);
 		len += rd;
-		if (!fd)
-			; // padding
-			// TODO extra shedules
+		iter = 0;
+		printf("rd: %lu, padded: %d\n", rd, padded);
+		print_bit_str("test1: ", buffer, 64);
+		if (rd == 0 && padded == 1)
+			return (0);
+		if (rd < BUFLEN)
+		{
+			rd += calculate_sha256_buf_padding(buffer, len);
+			padded = 1;
+			print_bit_str("test: ", buffer, 64);
+		}
 	}
-	while (rd >= 64)
+	printf("status1: iter:%lu, rd:%lu, padded:%d\n", iter, rd, padded);
+	if (iter < rd)
 	{
-		*block = buffer[BUFLEN - rd];
-		rd -= 64;
-		return (1);
+		*block = &buffer[iter];
+		iter += 64;
+		// rd -= 64;
 	}
-
-
+	print_bit_str("test: ", buffer, 64);
+	printf("status2: iter:%lu, rd:%lu, padded:%d\n", iter, rd, padded);
 	return (1);
 }
 
@@ -81,16 +81,19 @@ t_hash_sha256	calculate_sha256_from_file(const char *file_name)
 	char			*block_ptr;
 	int				ret;
 
-	fd = open(file_name, O_RDONLY);
 	init_sha256_hash(&hash);
+	fd = open(file_name, O_RDONLY);
 
 	int i = 0;
 	while ((ret = get_block_from_fd(fd, &block_ptr)))
 	{
 		calculate_sha256_block((reg32 *)block_ptr, &hash);
-
 		i++;
-		if (i == 10) exit(0);
+		if (i == 1000000)
+		{
+			printf("exit()\n");
+			exit(0);
+		}
 	}
 	return (hash);
 }
@@ -107,7 +110,7 @@ t_hash_sha256	calculate_sha256_from_stdin(void)
 		calculate_sha256_block((reg32 *)block_ptr, &hash);
 
 		i++;
-		if (i == 10) exit(0);
+		if (i == 100000) exit(0);
 	}
 	return (hash);
 }
