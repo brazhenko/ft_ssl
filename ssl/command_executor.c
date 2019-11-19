f4d23c7e884c50a3897a0b48775586805d85db29
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2019-11-19 19:55:00 +0300

    fast push

--- ssl/command_executor.c
+++ ssl/command_executor.c
@@ -18,9 +18,14 @@ int			command_executor(int ac, char *av[])
 		hash_executor(ac - 1, av + 1, &md5);
 	else if (strcmp(av[0], "sha256") == 0)
 		hash_executor(ac - 1, av + 1, &sha256);
+	else if (strcmp(av[0], "base64") == 0)
+	{
+		// encode_executor(ac - 1, av + 1, algo);
+		printf("debug\n");
+	}
 	else
 	{
-		nstrprint(13,
+		nstrprint(16,
 				"ft_ssl: Error: \'", av[0], "\' is an invalid command.\n",
 				"Standard commands:\n",
 				"\n"
@@ -30,7 +35,12 @@ int			command_executor(int ac, char *av[])
 				"sha512 - under development\n",
 				"Whirlpool - under development\n",
 				"\n",
-				"Cipher commands:\n", "\n");
+				"Cipher commands:\n",
+				"base64 - under development\n",
+				"des - under development\n",
+				"des-ecb - under development\n",
+				"des-cbc - under development\n"
+				"\n");
 	}
 	return (0);
 }
