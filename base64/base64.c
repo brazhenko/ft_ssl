921a4e9cbbb9f71fc9118086e4b518b5867f2c16
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2019-11-20 18:40:14 +0300

    +

--- base64/base64.c
+++ base64/base64.c
@@ -1,5 +1,25 @@
-void		*base64()
-{
+# define BASE64BUF_LEN 64
 
+void		*base64(char *str, int flags)
+{
+	return (NULL);
 }
 
+void 		*encode_fd_to_base64(int fd)
+{
+	return (NULL);
+}
+
+void		*encode_str_to_base64(char *str)
+{
+	t_reg32		tmp;
+	char		buf[BASE64BUF_LEN];
+	size_t 		i;
+
+	i = 0;
+	while (/* есть что обсчитывать */)
+	{
+		tmp = (t_reg32)str[i];
+		i += 3;
+	}
+}
