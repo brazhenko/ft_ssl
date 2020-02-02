293954e909ea173b3d9899f183a684010ade2de0
Andrew Brazhenko <andreybrazhenko@yandex.ru>
2020-02-02 18:54:14 +0300

    update norm 7

--- cipher/context/set_cipher_output_file.c
+++ cipher/context/set_cipher_output_file.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   set_cipher_output_file.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: a17641238 <a17641238@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/30 19:40:08 by a17641238         #+#    #+#             */
+/*   Updated: 2020/01/30 19:40:27 by a17641238        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cipher_context.h"
 #include <string.h>
 #include <fcntl.h>
@@ -8,7 +20,7 @@
 int					set_cipher_output_file(t_cipher_context *ctx,
 		const char *output_file_name)
 {
-	int 	output_fd;
+	int			output_fd;
 
 	memset(ctx->output_file, '\0', PATH_MAX);
 	strcpy(ctx->output_file, output_file_name);
