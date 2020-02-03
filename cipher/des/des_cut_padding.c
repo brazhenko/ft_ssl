8d038176a350d742902840431590bcb258c6f97d
lreznak- <andreybrazhenko@yandex.ru>
2020-02-03 19:18:24 +0300

    refactor and norm

--- cipher/des/des_cut_padding.c
+++ cipher/des/des_cut_padding.c
@@ -6,7 +6,7 @@
 /*   By: a17641238 <a17641238@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/30 17:38:23 by a17641238         #+#    #+#             */
-/*   Updated: 2020/02/03 18:58:28 by lreznak-         ###   ########.fr       */
+/*   Updated: 2020/02/03 19:16:19 by lreznak-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@
 static void	corrupted_last_block_exit(void)
 {
 	nstrprinterror(1, "last block corrupted...\bexit\n");
+	exit(EXIT_FAILURE);
 }
 
 size_t		des_cut_padding(t_cipher_context *ctx, t_lpdesblock block)
@@ -30,26 +31,23 @@ size_t		des_cut_padding(t_cipher_context *ctx, t_lpdesblock block)
 	size_t		i;
 	
 	if (memcmp(((unsigned char *)block) + 7, "\1", 1) == 0)
-		(*block)[(i = 7)] = 0;
+		return (7);
 	else if (memcmp(((unsigned char *)block) + 6, "\2\2", 2) == 0)
-		(*block)[(i = 6)] = 0;
+		return (6);
 	else if (memcmp(((unsigned char *)block) + 5, "\3\3\3", 3) == 0)
-		(*block)[(i = 5)] = 0;
+		return (5);
 	else if (memcmp(((unsigned char *)block) + 4, "\4\4\4\4", 4) == 0)
-		(*block)[(i = 4)] = 0;
+		return (4);
 	else if (memcmp(((unsigned char *)block) + 3, "\5\5\5\5\5", 5) == 0)
-		(*block)[(i = 3)] = 0;
+		return (3);
 	else if (memcmp(((unsigned char *)block) + 2, "\6\6\6\6\6\6", 6) == 0)
-		(*block)[(i = 2)] = 0;
+		return (2);
 	else if (memcmp(((unsigned char *)block) + 1, "\7\7\7\7\7\7\7", 7) == 0)
-		(*block)[(i = 1)] = 0;
+		return (1);
 	else if (memcmp(((unsigned char *)block) + 0,
 			"\10\10\10\10\10\10\10\10", 8) == 0)
-		(*block)[(i = 0)] = 0;
+		return (0);
 	else
-	{
-		i = 8;
 		corrupted_last_block_exit();
-	}
-	return (i);
+	return (0);
 }
